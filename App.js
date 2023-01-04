0f00c41f821f81791c5d64811e24e7aff5387b63
MarinaNam <namma123@naver.com>
2023-01-05 01:18:41 +0900

    feat: icon image적용

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Swiper from 'react-native-swiper';
+import styled, { css } from '@emotion/native';
+import {
+  DarkTheme,
+  NavigationContainer,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { LIGHT, DARK } from './colors';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const isDark = useColorScheme() === 'dark';
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={isDark ? DARK : LIGHT}>
+      <NavigationContainer style={styles.container}>
+        <Stack.Navigator>
+          <Text>영화 소개 사이트를 만들 것임!</Text>
+          <StatusBar style='auto' />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
---
+++ colors.js
@@ -0,0 +1,7 @@
+export const LIGHT = '#6a89cc';
+export const LIGHT_TEXT = '#b8e994';
+export const LIGHT_POINT = '#fad390';
+export const DARK = '#0c2461';
+export const DARK_TEXT = '#079992';
+export const DARK_POINT = '#b71540';
+export const SOMEWHERE = '#3c6382';
